e8d3b2c8613195e5234e428c677f921746e4da38
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 19:49:06 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in nederbörd/år från en fil och visa ett diagram
+        //  År     0         1         2         3 x 1000 mm
+        // 2011    ***************
+        // 2012    *************************
+        // 2013    ************
+
+
+    }
+}
