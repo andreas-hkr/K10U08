3a319414682b254f1ccba8c084d61923349cd5a0
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 19:58:23 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Läs in nederbörd/år från en fil och visa ett diagram
         //  År     0         1         2         3 x 1000 mm
         // 2011    ***************
         // 2012    *************************
         // 2013    ************
 
+        Scanner fileIn = new Scanner(new File("data.txt"));
 
+        System.out.println(" År    0         1         2         3 x 1000 mm");
+        while (fileIn.hasNextLine()) {
+            int year = fileIn.nextInt();
+            int amount = fileIn.nextInt();
+            amount /= 100;
+
+            System.out.print(year + "   ");
+            for(int i=0; i <= amount; i++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
     }
 }
